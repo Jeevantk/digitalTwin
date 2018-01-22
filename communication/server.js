0f269a4f7d43bf506a62a6afa0a5b357dd7cf776
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-01-22 18:11:01 +0530

    Added HTML as a template in the server.js file

--- communication/server.js
+++ communication/server.js
@@ -7,9 +7,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+// });
+
 
 
 app.get('/ui/style.css', function (req, res) {
@@ -35,25 +36,55 @@ function onData(data){
     currentValue=data;
 }
 
+function createTemplate(currentValue){
+
+	var currentNumber=currentValue;
+	var htmlTemplate=
+		`<!doctype html>
+			<html>
+			    <head>
+			        <link href="/ui/style.css" rel="stylesheet" />
+			    </head>
+			    <body>
+			        <div class="center">
+			            <img src="/ui/madi.png" class="img-medium"/>
+			        </div>
+			        <br>
+			        <div class="center text-big bold">
+			            Sample Demo for Communication for Digital Twin using arduino and Nodejs ${currentNumber}
+			        </div>
+
+			        <script type="text/javascript" src="/ui/main.js">
+			        </script>
+			    </body>
+			</html>
+	`;
+
+	return htmlTemplate;
+}
+
+app.get('/',function(req,res){
+	res.send(createTemplate(currentValue));
+});
+
+
 myPort.on('open', onOpen);
 myPort.on('data', onData);
 
-// var port = 8081; // Use 8080 for local development because you might already have apache running on 80
-// app.listen(8081, function () {
-//   console.log(`Digital Twin Listening on port ${port}!`);
-// });
+var port = 8081; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8081, function () {
+  console.log(`Digital Twin Listening on port ${port}!`);
+});
 
 
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(currentValue);
-}).listen(8081);
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end(currentValue);
+// }).listen(8081);
 
 console.log('Server running at http://127.0.0.1:8081');
-
-
 
 
 
