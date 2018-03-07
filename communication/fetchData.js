521043813008f268426f9ff6ad6c4c66b271f53d
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-03-07 16:46:39 +0530

    Fixed Fetch data

--- communication/fetchData.js
+++ communication/fetchData.js
@@ -32,7 +32,7 @@ async function createTable() {
     let promise = new Promise((resolve, reject) => {
         console.log("Entered Create Table");
         db.run('CREATE TABLE IF NOT EXISTS tempurature(ID INTEGER PRIMARY KEY,tempurature TEXT)');
-        // return resolve()
+        return resolve("Done");
     });
   
     let result = await promise; // wait till the promise resolves (*)
@@ -44,23 +44,25 @@ createTable().then((result)=>{
     console.log("First Then ");
 })
 
-// createTable().then((result)=>{
-//     console.log("Entered first then");
-//     db.each("SELECT ID FROM tempurature ORDER BY ID DESC LIMIT 1", function(err, row) {  
-// 		insertId=row.ID+1;
-// 		console.log("From DB function User id  : "+row.ID);  
-// 	}).then((result)=>{
-//         var stmt = db.prepare("INSERT INTO tempurature VALUES (?,?)");
-//         console.log("User id : "+insertId);
-//         myPort.on('open', onOpen);
-//         myPort.on('data', function (data) {
-//             currentValue=data.toString('utf8');
-//             stmt.run(insertId,currentValue);
-//             insertId=insertId+1;
-//              console.log('Data:',currentValue);
+createTable().then((result)=>{
+    console.log("Entered first then");
+    db.each("SELECT ID FROM tempurature ORDER BY ID DESC LIMIT 1", function(err, row) {  
+		insertId=row.ID+1;
+		console.log("From DB function User id  : "+row.ID);  
+    });
+    })
+    .then((result)=>{
+        var stmt = db.prepare("INSERT INTO tempurature VALUES (?,?)");
+        console.log("User id : "+insertId);
+        myPort.on('open', onOpen);
+        myPort.on('data', function (data) {
+            currentValue=data.toString('utf8');
+            stmt.run(insertId,currentValue);
+            insertId=insertId+1;
+            console.log('Data:',currentValue);
+            console.log('Insert ID: ' ,insertId);
         
-//         });
-//     });
-// });
+        });
+    });
 
 
