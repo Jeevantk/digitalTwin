c9be1ec914150d4e6aab514949bfa61c38a89e56
jeevantk <jeevan.thomaskoshy@gmail.com>
2018-05-09 13:02:56 +0530

    adding read current

---
+++ arduino/readcurrent/readcurrent.ino
@@ -0,0 +1,45 @@
+ /*
+  ReadAnalogVoltage
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
+  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+*/
+int motpin=13;
+bool running = false;
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(motpin, OUTPUT);
+  digitalWrite(motpin, LOW);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if (Serial.available() > 0)
+  {
+    switch (Serial.read())
+    {
+      case '3':
+        digitalWrite(motpin, LOW);
+        break;
+      case '0':
+        digitalWrite(motpin, HIGH);
+        break;
+      case '1':
+        running = true;
+        Serial.print('\n');
+        break;
+      case '2':
+        running = false;
+        break;
+    }
+  }
+
+  if (running)
+  {
+    int sensorValue = analogRead(A0);
+    Serial.println(sensorValue);
+  }
+}
